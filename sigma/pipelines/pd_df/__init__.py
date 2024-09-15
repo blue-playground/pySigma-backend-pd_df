dd6916b96196f2fb4a3c9fd0f4e020f7a7fb660e
sagarwal <shubham.ag.452@gmail.com>
2024-09-15 08:14:34 +0000

    removing pipeline specificities

--- sigma/pipelines/pd_df/__init__.py
+++
@@ -1,6 +0,0 @@
-from .pd_df import pd_df_pipeline
-# TODO: add all pipelines that should be exposed to the user of your backend in the import statement above.
-
-pipelines = {
-    "pd_df_pipeline": pd_df_pipeline,   # TODO: adapt identifier to something approproiate
-}
