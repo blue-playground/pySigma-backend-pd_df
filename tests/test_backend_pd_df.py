467dcd940cdc0090d737a73252dfe13d938a4c1a
sagarwal <shubham.ag.452@gmail.com>
2024-09-19 08:16:26 +0000

    Implement 3 tests

--- tests/test_backend_pd_df.py
+++ tests/test_backend_pd_df.py
@@ -1,6 +1,11 @@
+import sys
+import os
+
+sys.path.append(os.getcwd() + "/sigma")
+
 import pytest
 from sigma.collection import SigmaCollection
-from sigma.backends.pd_df import PandasDataFramePythonBackend
+from backends.pd_df import PandasDataFramePythonBackend
 
 
 @pytest.fixture
@@ -8,7 +13,7 @@ def pd_df_backend():
     return PandasDataFramePythonBackend()
 
 
-# TODO: implement tests for some basic queries and their expected results.
+# DONE: implement tests for some basic queries and their expected results.
 def test_pd_df_and_expression(pd_df_backend: PandasDataFramePythonBackend):
     assert (
         pd_df_backend.convert(
@@ -27,7 +32,7 @@ def test_pd_df_and_expression(pd_df_backend: PandasDataFramePythonBackend):
         """
             )
         )
-        == ["df[(df['fieldA'] == 'valueA') & (df['fieldB'] == 'valueB')]"]
+        == ["""df.query("fieldA=='valueA' and fieldB=='valueB'")"""]
     )
 
 
@@ -50,7 +55,7 @@ def test_pd_df_or_expression(pd_df_backend: PandasDataFramePythonBackend):
         """
             )
         )
-        == ["<insert expected result here>"]
+        == ["""df.query("fieldA=='valueA' or fieldB=='valueB'")"""]
     )
 
 
@@ -76,122 +81,119 @@ def test_pd_df_and_or_expression(pd_df_backend: PandasDataFramePythonBackend):
         """
             )
         )
-        == ["<insert expected result here>"]
+        == [
+            """df.query("(fieldA==['valueA1','valueA2']) and (fieldB==['valueB1','valueB2'])",inplace=True)"""
+        ]
     )
 
+    # def test_pd_df_or_and_expression(pd_df_backend: PandasDataFramePythonBackend):
+    #     assert (
+    #         pd_df_backend.convert(
+    #             SigmaCollection.from_yaml(
+    #                 """
+    #             title: Test
+    #             status: test
+    #             logsource:
+    #                 category: test_category
+    #                 product: test_product
+    #             detection:
+    #                 sel1:
+    #                     fieldA: valueA1
+    #                     fieldB: valueB1
+    #                 sel2:
+    #                     fieldA: valueA2
+    #                     fieldB: valueB2
+    #                 condition: 1 of sel*
+    #         """
+    #             )
+    #         )
+    #         == ["<insert expected result here>"]
+    #     )
 
-def test_pd_df_or_and_expression(pd_df_backend: PandasDataFramePythonBackend):
-    assert (
-        pd_df_backend.convert(
-            SigmaCollection.from_yaml(
-                """
-            title: Test
-            status: test
-            logsource:
-                category: test_category
-                product: test_product
-            detection:
-                sel1:
-                    fieldA: valueA1
-                    fieldB: valueB1
-                sel2:
-                    fieldA: valueA2
-                    fieldB: valueB2
-                condition: 1 of sel*
-        """
-            )
-        )
-        == ["<insert expected result here>"]
-    )
+    # def test_pd_df_in_expression(pd_df_backend: PandasDataFramePythonBackend):
+    #     assert (
+    #         pd_df_backend.convert(
+    #             SigmaCollection.from_yaml(
+    #                 """
+    #             title: Test
+    #             status: test
+    #             logsource:
+    #                 category: test_category
+    #                 product: test_product
+    #             detection:
+    #                 sel:
+    #                     fieldA:
+    #                         - valueA
+    #                         - valueB
+    #                         - valueC*
+    #                 condition: sel
+    #         """
+    #             )
+    #         )
+    #         == ["<insert expected result here>"]
+    #     )
 
+    # def test_pd_df_regex_query(pd_df_backend: PandasDataFramePythonBackend):
+    #     assert (
+    #         pd_df_backend.convert(
+    #             SigmaCollection.from_yaml(
+    #                 """
+    #             title: Test
+    #             status: test
+    #             logsource:
+    #                 category: test_category
+    #                 product: test_product
+    #             detection:
+    #                 sel:
+    #                     fieldA|re: foo.*bar
+    #                     fieldB: foo
+    #                 condition: sel
+    #         """
+    #             )
+    #         )
+    #         == ["<insert expected result here>"]
+    #     )
 
-def test_pd_df_in_expression(pd_df_backend: PandasDataFramePythonBackend):
-    assert (
-        pd_df_backend.convert(
-            SigmaCollection.from_yaml(
-                """
-            title: Test
-            status: test
-            logsource:
-                category: test_category
-                product: test_product
-            detection:
-                sel:
-                    fieldA:
-                        - valueA
-                        - valueB
-                        - valueC*
-                condition: sel
-        """
-            )
-        )
-        == ["<insert expected result here>"]
-    )
+    # def test_pd_df_cidr_query(pd_df_backend: PandasDataFramePythonBackend):
+    #     assert (
+    #         pd_df_backend.convert(
+    #             SigmaCollection.from_yaml(
+    #                 """
+    #             title: Test
+    #             status: test
+    #             logsource:
+    #                 category: test_category
+    #                 product: test_product
+    #             detection:
+    #                 sel:
+    #                     field|cidr: 192.168.0.0/16
+    #                 condition: sel
+    #         """
+    #             )
+    #         )
+    #         == ["<insert expected result here>"]
+    #     )
 
-
-def test_pd_df_regex_query(pd_df_backend: PandasDataFramePythonBackend):
-    assert (
-        pd_df_backend.convert(
-            SigmaCollection.from_yaml(
-                """
-            title: Test
-            status: test
-            logsource:
-                category: test_category
-                product: test_product
-            detection:
-                sel:
-                    fieldA|re: foo.*bar
-                    fieldB: foo
-                condition: sel
-        """
-            )
-        )
-        == ["<insert expected result here>"]
-    )
-
-
-def test_pd_df_cidr_query(pd_df_backend: PandasDataFramePythonBackend):
-    assert (
-        pd_df_backend.convert(
-            SigmaCollection.from_yaml(
-                """
-            title: Test
-            status: test
-            logsource:
-                category: test_category
-                product: test_product
-            detection:
-                sel:
-                    field|cidr: 192.168.0.0/16
-                condition: sel
-        """
-            )
-        )
-        == ["<insert expected result here>"]
-    )
-
-
-def test_pd_df_field_name_with_whitespace(pd_df_backend: PandasDataFramePythonBackend):
-    assert (
-        pd_df_backend.convert(
-            SigmaCollection.from_yaml(
-                """
-            title: Test
-            status: test
-            logsource:
-                category: test_category
-                product: test_product
-            detection:
-                sel:
-                    field name: value
-                condition: sel
-        """
-            )
-        )
-        == ["<insert expected result here>"]
-    )
+    # def test_pd_df_field_name_with_whitespace(pd_df_backend: PandasDataFramePythonBackend):
+    # assert (
+    #     pd_df_backend.convert(
+    #         SigmaCollection.from_yaml(
+    #             """
+    #         title: Test
+    #         status: test
+    #         logsource:
+    #             category: test_category
+    #             product: test_product
+    #         detection:
+    #             sel:
+    #                 field name: value
+    #             condition: sel
+    #     """
+    #         )
+    #     )
+    #     == ["<insert expected result here>"]
+    # )
 
 
-# TODO: implement tests for all backend features that don't belong to the base class defaults, e.g. features that were
+# TODOOODDD: implement tests for all backend features that don't belong to the base class defaults, e.g. features that were
 # implemented with custom code, deferred expressions etc.
